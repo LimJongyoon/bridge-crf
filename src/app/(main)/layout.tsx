8f178b557670962b2807dda867c3167bddf8fb40
LimJongyoon <55519519+LimJongyoon@users.noreply.github.com>
2025-06-09 16:54:35 +0900

    inter삭제 2

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,6 @@
 
 import "../globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import MobileHeader from "./MobileHeader";
 
 export const metadata: Metadata = {
